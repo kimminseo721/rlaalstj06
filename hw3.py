23ea0b75edc516e16f4a70443bb8c06647ecf46b
kimminseo721 <gon522go@gmail.com>
2025-03-31 20:02:37 +0900

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,11 +1,13 @@
-import getfixedprice as gfp
+def get_fixed_price(price, rate):
+    result = int(price / ((100 - rate)/100))
+    return result
 
 def main():
     r = int(input('할인율은? '))
     p1 = int(input('A 상품의 할인된 가격은? '))
     p2 = int(input('B 상품의 할인된 가격은? '))
-    A = gfp.get_fixed_price(p1, r)
-    B = gfp.get_fixed_price(p2, r)
+    A = get_fixed_price(p1, r)
+    B = get_fixed_price(p2, r)
     print('A상품의 정가는', A, '원')
     print('B상품의 정가는', B, '원')
 
